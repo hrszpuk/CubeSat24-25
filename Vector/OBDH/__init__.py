ac69072bbfacabb415b955d4216edcd7acb1595c
psybj5 <psybj5@nottingham.ac.uk>
2025-06-14 22:21:24 +0100

    manual mode yeheheeh

--- Vector/OBDH/__init__.py
+++ Vector/OBDH/__init__.py
@@ -1,15 +1,26 @@
 import Vector.ADCS as adcs
 import multiprocessing as mp
 
-def start():
+def start(manual = False):
     pipeMain, pipeChild = mp.Pipe()
     p = mp.Process(target=adcs.start, args=(pipeChild,))
     p.start()
 
-    pipeMain.send("health_check")
-    print(pipeMain.recv())
-
-    pipeMain.send("stop")
+    # TEST CODE
+    if not manual:
+        pipeMain.send("health_check")
+        print(pipeMain.recv())
+        pipeMain.send("stop")
+    else:
+        running = True
+        while running:
+            userInput = input("-> ")
+            if userInput == "stop":
+                running = False
+            elif userInput == "health_check":
+                pipeMain.send("health_check")
+                print(pipeMain.recv())
+                pipeMain.send("stop")
 
     p.join()
     pipeMain.close()
---
+++ Vector/manual.py
@@ -0,0 +1,7 @@
+"""
+Start up script
+"""
+from OBDH import start
+
+if __name__ == '__main__':
+    start(True)
