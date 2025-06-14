d3b072b02b76c47cee594663fff56526949ab086
tomas-ribeiro-pinto <morato.toms@outlook.com>
2025-06-15 00:16:42 +0100

    added support for distance measurement

--- Vector/Payload/distance_sensor.py
+++ Vector/Payload/distance_sensor.py
@@ -1,6 +1,29 @@
-class DistanceSensor:
-    def __init__(self, distance):
-        self.distance = distance
+import time
+import board
+import adafruit_vl53l4cd
 
-    def get_distance(self):
-        return self.distance
+i2c = board.I2C()
+vl53 = adafruit_vl53l4cd.VL53L4CD(i2c)
+
+vl53.timing_budget = 200  # Adjust for range vs. speed
+vl53.start_ranging()
+
+try:
+    while True:
+        while not vl53.data_ready:
+            pass
+        vl53.clear_interrupt()
+        
+        distance = vl53.distance
+        
+        if distance == 0:
+            print("No target detected!")
+        elif distance >= 8190:
+            print("Sensor saturated (too bright or reflective)")
+        else:
+            print(f"Distance: {distance} mm")
+        
+        time.sleep(0.1)
+        
+except KeyboardInterrupt:
+    vl53.stop_ranging()
