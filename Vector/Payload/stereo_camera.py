c93db7ed27d636b581fdb7ec1a9097972d9d0e1a
tomas-ribeiro-pinto <morato.toms@outlook.com>
2025-06-15 01:06:55 +0100

    removed copy of stereo camera

--- Vector/Payload/stereo_camera.py
+++ Vector/Payload/stereo_camera.py
@@ -1,28 +1,18 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
- 
-# imgL = cv.imread('tsukuba_l.png', cv.IMREAD_GRAYSCALE)
-# imgR = cv.imread('tsukuba_r.png', cv.IMREAD_GRAYSCALE)
- 
-# stereo = cv.StereoBM.create(numDisparities=16, blockSize=15)
-# disparity = stereo.compute(imgL,imgR)
-# plt.imshow(disparity,'gray')
-# plt.show()
+from camera import Camera
 
 class StereoCamera:
-    def __init__(self, left_camera, right_camera):
-        self.left_camera = left_camera
-        self.right_camera = right_camera
+    def __init__(self):
+        self.left_camera = Camera(camera_index=0)
+        self.right_camera = Camera(camera_index=1)
 
     def get_left_image(self):
-        return self.left_camera.get_image()
+        return self.left_camera.get_frame()
 
     def get_right_image(self):
-        return self.right_camera.get_image()
-
-    def get_stereo_images(self):
-        return self.get_left_image(), self.get_right_image()
+        return self.right_camera.get_frame()
     
     def get_depth_map(self):
         left_image = self.get_left_image()
@@ -38,5 +28,8 @@ class StereoCamera:
 
         stereo = cv.StereoBM.create(numDisparities=16, blockSize=15)
         disparity = stereo.compute(imgL,imgR)
+
+        plt.imshow(disparity,'gray')
+        plt.show()
 
         return disparity
