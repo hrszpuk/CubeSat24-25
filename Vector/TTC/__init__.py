da27a55f756793b8dbf315ce1bdc71656d96d520
hfytl2 <hfytl2@nottingham.edu.my>
2025-06-29 05:16:28 +0100

    Update TT&C

--- Vector/TTC/__init__.py
+++ Vector/TTC/__init__.py
@@ -1,58 +1,11 @@
 import asyncio
 from TTC.main import TTC
-from TTC.process_manager import ProcessManager
 
-processes = ProcessManager()
 event_loop = asyncio.get_event_loop()
 
-def start(obdh_pipe, log_queue):
-    ttc = TTC(obdh_pipe, log_queue)
+def start(pipe, log_queue):
+    ttc = TTC(pipe, event_loop, log_queue)
     ttc.log("Starting subsystem...")
-    processes.set_logfn(ttc.log)
-    processes.start("ground_communications", ground_comms, ttc, obdh_pipe)
-    processes.start("obdh_communications", obdh_comms, ttc, obdh_pipe)
-
-def ground_comms(ttc, obdh_pipe):
+    ttc.start_pipe_listener()
     event_loop.run_until_complete(ttc.start_server())
     event_loop.run_forever()
-
-def obdh_comms(ttc, obdh_pipe):
-    running = True
-
-    while running:
-        response = obdh_pipe.recv()
-        command = response[0]
-        args = response[1] if len(response) > 1 else {}
-        ttc.log(f"Received command from OBDH: {command} with arguments {args}")
-
-        match command:
-            case "log":
-                message = args.get("message", "")
-                ttc.log(message)
-            case "health_check":
-                try:
-                    health_check = ttc.health_check()
-                    obdh_pipe.send(health_check)
-                except Exception as err:
-                    ttc.log(f"[ERROR] Failed to process command ({command}) from OBDH: {err}")
-            case "send_message":
-                msg = args["message"]
-                
-                try:
-                    ttc.send_message(msg)
-                except Exception as err:
-                    ttc.log(f"[ERROR] Failed to process command ({command}) from OBDH: {err}")
-            case "send_file":
-                path = args["path"]
-
-                try:
-                    ttc.send_file(path)
-                except Exception as err:
-                    ttc.log(f"[ERROR] Failed to process command ({command}) from OBDH: {err}")
-            case "stop":
-                ttc.log("Stopping subprocesses and shutting down...")
-                processes.shutdown()
-                running = False
-                ttc.log("Successfully shut down")
-            case _:
-                ttc.log(f"Invalid command received from OBDH: {command}")
--- Vector/TTC/testttc.py
+++ Vector/TTC/testttc.py
@@ -3,23 +3,15 @@ import socket
 import websockets
 import json
 import base64
-from enums import TTCState, MessageType
 from datetime import datetime
-from TTC.utils import get_connection_info
-import threading
-import asyncio
 
-class TTC:
-    def __init__(self, pipe, event_loop, log_queue, port=8000, buffer_size=1024, format="utf-8", byteorder_length=8, max_retries=3):
-        log_queue.put(("TT&C", "Initialising..."))
+class TestTTC:
+    def __init__(self, event_loop, port=8000, buffer_size=1024, format="utf-8", byteorder_length=8, max_retries=3):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 0))
 
         # module configuration
-        self.state = TTCState.INITIALISING
-        self.pipe = pipe
         self.event_loop = event_loop
-        self.log_queue = log_queue
         self.BUFFER_SIZE = buffer_size
         self.FORMAT = format
         self.BYTEORDER_LENGTH = byteorder_length
@@ -32,66 +24,27 @@ class TTC:
         self.connection = None
         self.last_command_received = None
 
-        log_queue.put(("TT&C", "Initialised"))
-
     def log(self, msg):
-        self.log_queue.put(("TT&C", msg))
-
-    def start_pipe_listener(self):
-        def loop_process():
-            self.log("Starting OBDH listener thread...")
-
-            while True:
-                if self.pipe.poll():
-                    instruction = self.pipe.recv()
-                    command = instruction[0]
-                    args = instruction[1] if len(instruction) == 2 else None
-                    self.log(f"Received command: {command} with args: {args} from OBDH")
-                    pipe_loop = asyncio.get_event_loop()
-
-                    match command:
-                        case "stop":
-                            self.log("OBDH listener shutting down...")
-                            self.event_loop.call_soon_threadsafe(self.event_loop.stop)
-                            break
-                        case "log":
-                            asyncio.run_coroutine_threadsafe(self.send_log(args["message"]), pipe_loop)
-                        case "send_message":
-                            asyncio.run_coroutine_threadsafe(self.send_message(args["message"]), pipe_loop)
-                        case "send_file":
-                            asyncio.run_coroutine_threadsafe(self.send_file(args["path"]), pipe_loop)
-                        case "health_check":
-                            health = self.health_check()
-                            self.pipe.send(health)
-                        case _:
-                            self.log(f"Invalid instruction received from OBDH: {command}")
-
-        t = threading.Thread(target=loop_process, daemon=True)
-        t.start()
+        print(msg)
 
     async def start_server(self):
         try:
             await websockets.serve(self.handle_connection, self.ip, self.port)
             self.log(f"Listening for connections on {self.host_name} ({self.ip}:{self.port})")
-            self.state = TTCState.READY
-            self.pipe.send(self.state == TTCState.READY)
-            self.log(f"Ready")
         except Exception as e:
             self.log(f"[ERROR] Could not start WebSocket server: {e}")
 
     async def handle_connection(self, connection):
         self.connection = connection
-        self.state = TTCState.CONNECTED
         self.last_command_received = datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT")
         self.log(f"Connection established with {self.connection.remote_address[0]}:{self.connection.remote_address[1]}")
 
-        while self.state == TTCState.CONNECTED:
+        while True:
             try:
                 await self.handle_message()
             except websockets.exceptions.ConnectionClosed:
                 self.log(f"Connection with {self.connection.remote_address[0]}:{self.connection.remote_address[1]} dropped")
                 self.connection = None
-                self.state = TTCState.READY
                 break
             except Exception as e:
                 self.log(f"[ERROR] WebSocket connection handler failed: {e}")
@@ -106,8 +59,7 @@ class TTC:
         self.log(f"Sending \"{message}\" to Ground...")
 
         try:
-            await self.connection.send(json.dumps({"type": MessageType.LOG.name.lower(), "data": message}))
-            self.log(f"Sent \"{message}\" to Ground")
+            await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": "log", "data": message}))
         except Exception as err:
             self.log(f"[ERROR] Failed to send \"{message}\": {err}")
 
@@ -115,7 +67,7 @@ class TTC:
         self.log(f"Sending \"{message}\" to Ground...")
 
         try:
-            await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": MessageType.MESSAGE.name.lower(), "data": message}))
+            await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": "message", "data": message}))
             self.log(f"Sent \"{message}\" to Ground")
         except Exception as err:
             self.log(f"[ERROR] Failed to send \"{message}\": {err}")
@@ -129,11 +81,11 @@ class TTC:
 
         match command:
             case "ping":
-                await self.send_message("pong")
                 pass
             case "get_file":
                 if arguments:
                     path = arguments[0]
+                    await self.send_message(f"Sending file {path}...")
                     await self.send_file(path)
                 else:
                     await self.send_message("No file path provided!")
@@ -158,7 +110,6 @@ class TTC:
                     await self.send_message("No phase provided!")
             case "shutdown":
                 await self.send_message("Shutting down...")
-                self.pipe.send("shutdown")
             case _:
                 self.log(f"[ERROR] Invalid command received: {command}")
                 await self.send_message(f"{command} is not a valid command!")
@@ -177,14 +128,14 @@ class TTC:
                 file_base_name = os.path.basename(file_path)
                 file_size = os.path.getsize(file_path)
                 self.log("Sending file metadata...")
-                await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": MessageType.FILEMETADATA.name.lower(), "data": {"size": file_size, "name": file_base_name}}))
+                await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": "filemetadata", "data": {"size": file_size, "name": file_base_name}}))
                 self.log("Sent file metadata")
 
                 with open(file_path, "rb") as f:
                     self.log("Sending file data...")
 
                     while chunk := f.read(self.BUFFER_SIZE):
-                        await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": MessageType.FILEDATA.name.lower(), "data": base64.b64encode(chunk).decode("ascii")}))
+                        await self.connection.send(json.dumps({"timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S GMT"), "type": "filedata", "data": base64.b64encode(chunk).decode("ascii")}))
 
                     await self.send_message("File send complete")
                     self.log("Sent file data")
@@ -213,28 +164,3 @@ class TTC:
     
     def get_connection(self):
         return self.connection
-
-    def health_check(self):
-        self.log("Performing subsystem health check...")
-        health_check = {}
-        connection_info = get_connection_info()
-
-        for metric, value in connection_info.items():
-            if value is not None:
-                item_str = f"{abs(value)}"
-
-                if "Frequency" in metric:
-                    item_str += " GHz"
-                elif metric == "Signal Strength":
-                    item_str += " dBm"
-                elif metric == "Data Transmission Rate":
-                    item_str += " Mb/s"
-            else:
-                item_str = None
-            
-            health_check[metric] = item_str
-
-        health_check["Last Command Received"] = self.last_command_received
-        self.log(f"Subsystem health check: {health_check}")
-
-        return health_check
