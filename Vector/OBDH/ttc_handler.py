546ea396845d63c00dbe70fdbde843bb3112b8f0
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2025-07-02 15:16:26 +0100

    Updated TTCHandler to accept any command not just log

--- Vector/OBDH/ttc_handler.py
+++ Vector/OBDH/ttc_handler.py
@@ -2,13 +2,14 @@ import logging
 from multiprocessing import *
 
 class TTCHandler(logging.Handler):
-    def __init__(self, pipe_conn):
+    def __init__(self, pipe_conn, command):
         super().__init__()
         self.pipe_conn = pipe_conn
+        self.command = command
 
     def emit(self, record):
         try:
             s = self.format(record)
-            self.pipe_conn.send(("log", {"message": s}))
+            self.pipe_conn.send((self.command, {"message": s}))
         except Exception:
             self.handleError(record)
